5c4a5f209dbc36bd730db70370a4710347649c1c
matteo.elguetibi.pro@gmail.com <test@debian>
2021-12-01 14:46:26 +0100

    formulaire

--- index.php
+++ index.php
@@ -8,16 +8,37 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="css_cven.css" type="text/css" >
     </head>
     <body>
+        <h1>Site du jura</h1>
         <form>
-            <label for="nom">Nom</label>
-            <input type="text" name="nom" id="nom">
-            <label for="nom">Prenom</label>
-            <input type="text" name="prenom" id="prenom">
-            
+            <p class="tform">informattion personnelles</p>
+            <label for="nom">Nom
+                <input type="text" name="nom" id="nom">
+            </label>
+            <label for="prenom">Prenom
+                <input type="text" name="prenom" id="prenom">
+            </label>
+            <p class="tform">Choississez le type de reservation</p>
+            <label for="reservation" >logements
+                <input type="radio" name="reservation" id="logements" value="logements"></label>
+            <label for="reservation">chambres doubles
+                <input type="radio" name="reservation" id="chambres_doubles" value="chambres_doubles"></label>
+            <label for="reservation">chambres de 3 lits séparés
+                <input type="radio" name="reservation" id="3lit_separe" value="3lit_separe"></label>
+            <label for="reservation">chambres de 4 lits séparés
+                <input type="radio" name="reservation" id="4lit_separe" value="4lit_separe"></label>
+            <label for="reservation">logement adapté pour les personnes à mobilité réduite.
+                <input type="radio" name="reservation" id="log_mobilite" value="log_mobilite"></label>
+            <br/>
+            <p class="tform">Voulez vous beneficier de la pension complète</p>
+            <label for="pension_complete">oui
+                <input type="radio" name="pension_compete" id=oui" value="1">
+            </label>
+            <label for="pension_complete">non
+                <input type="radio" name="pension_compete" id="non" value="0">
+            </label>
         </form>
-        <?php
-        ?>
     </body>
 </html>

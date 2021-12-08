6ef6f7c3d9f3571da7b1c14dcc7aecef837359e0
ChrisMBA <80053919+ChrisMBA@users.noreply.github.com>
2021-12-08 14:38:11 +0100

    Update BDD_CVEN.sql

--- BDD_CVEN.sql
+++ BDD_CVEN.sql
@@ -5,13 +5,13 @@ drop table if exists Hebergement_Restauration;
 
 
 create table Client (
-  ID_utilisateur int not null primary key auto_increment,
+  ID_utilisateur int(1) not null primary key auto_increment,
   Login varchar(100) not null,
   Mdp varchar(100) not null,
   Nom varchar(100) not null,
   Prenom varchar(100) not null,
   Administrateur boolean not null
-  ) ENGINE = InnoDB ;
+  );
 
 create table Reservation (
   ID_reservation int not null primary key auto_increment,
@@ -20,11 +20,11 @@ create table Reservation (
   Nombre_Client int not null,
   Restaurant boolean not null,
   Menage boolean not null,
-  ) ENGINE = InnoDB ;
+  );
 
 create table Attribuer_Hebergement (
   Date_hebergement date not null
-  ) ENGINE = InnoDB ;
+  );
 
 create table Hebergement_Restauration (
   ID_hebergement int not null primary key auto_increment,
@@ -32,39 +32,40 @@ create table Hebergement_Restauration (
   Emplacement varchar(100) not null,
   Etage int not null,
   Commentaire varchar(100) not null
-  ) ENGINE = InnoDB ;
+  ); 
 
-INSERT INTO 'Client' (`Login`,`Mdp`,`Nom`,`Prenom`,`Administrateur`)
+INSERT INTO Client (ID_utilisateur,Login,Mdp,Nom,Prenom,Administrateur)
 VALUES
-  ("13525","ECB69KXG7QA","Olson","Tiger","No"),
-  ("35045","PTE42SGS6SL","Beach","Ryan","Yes"),
-  ("57473","UUY03WGP3EF","Alexander","Griffith","Yes"),
-  ("17718","HSO34GTE1YB","Stein","Cherokee","Yes"),
-  ("37061","HNM16LUH5ML","Conway","Yvette","Yes");
+  (1,"13525","ECB69KXG7QA","Olson","Tiger","0"),
+  (2,"35045","PTE42SGS6SL","Beach","Ryan","1"),
+  (3,"57473","UUY03WGP3EF","Alexander","Griffith","0"),
+  (4,"17718","HSO34GTE1YB","Stein","Cherokee","1"),
+  (5,"37061","HNM16LUH5ML","Conway","Yvette","0"
+);
 
-
-INSERT INTO `Reservation` (`Date_Arrivee`,`Date_Depart`,`Nombre_Client`,`Restaurant`,`Menage`)
+INSERT INTO Reservation (ID_reservation,Date_Arrivee,Date_Depart,Nombre_Client,Restaurant,Menage)
 VALUES
-  ("01-10-20","02-11-21",6,"Yes","No"),
-  ("01-15-20","02-06-21",44,"No","No"),
-  ("01-27-20","02-18-21",7,"No","No"),
-  ("01-20-20","02-08-21",18,"No","No"),
-  ("01-19-20","02-05-21",15,"No","Yes");
+  (1,"01-10-20","02-11-21",6,"0","1"),
+  (2,"01-15-20","02-06-21",44,"1","0"),
+  (3,"01-27-20","02-18-21",7,"0","0"),
+  (4,"01-20-20","02-08-21",18,"0","0"),
+  (5,"01-19-20","02-05-21",15,"0","1"
+);
 
-
-INSERT INTO `Attribuer_Hebergement` (`Date_hebergement`)
+INSERT INTO Attribuer_Hebergement (Date_hebergement)
 VALUES
-  ("Feb 20, 2020"),
-  ("Feb 13, 2020"),
-  ("Feb 3, 2020"),
-  ("Jan 5, 2020"),
-  ("Jan 17, 2020");
+  ("02-10-20"),
+  ("01-15-20"),
+  ("01-15-20"),
+  ("01-15-20"),
+  ("01-15-20"
+);
 
-
-INSERT INTO `Hebergement_Restauration` (`Type_hebergement`,`Emplacement`,`Etage`,`Commentaire`)
+INSERT INTO Hebergement_Restauration (ID_hebergement,Type_hebergement,Emplacement,Etage,Commentaire)
 VALUES
-  ("ullamcorper, nisl arcu","sapien. Nunc pulvinar",1,"incroayfzg"),
-  ("mauris erat eget","semper rutrum. Fusce",1,"genial"),
-  ("eget magna. Suspendisse","eros. Proin ultrices.",3,"hteahjta"),
-  ("Nam porttitor scelerisque","a ultricies adipiscing,",1,"gfrahatreh"),
-  ("felis. Nulla tempor","Sed et libero.",3,"gqggnulrfg");
+  (1,"chambres avec lit double","",1,"incroayfzg"),
+  (2,"chambre simple","semper rutrum. Fusce",1,"genial"),
+  (3,"chambre avec balcon", "Suspendisse","eros. Proin ultrices.",3,"hteahjta"),
+  (4,"chambre simple","a ultricies adipiscing,",1,"gfrahatreh"),
+  (5,"chambre avec lit double ","Sed et libero.",3,"gqggnulrfg"
+);
